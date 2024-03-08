a51dbfdf0914d7eb83d37ffe9749fdcaf2800f31
HuenBasset <hu351bas@htwg-konstanz.de>
2024-03-08 16:30:29 +0100

    Final Todo App

---
+++ src/data/todos.ts
@@ -0,0 +1,19 @@
+import { Todo } from "../types/todo";
+
+export const dummyData: Todo[] = [
+    {
+        id: 1,
+        title: "This is the first todo.",
+        completed: false,
+    },
+    {
+        id: 2,
+        title: "This is the second todo.",
+        completed: false,
+    },
+    {
+        id: 3,
+        title: "This is the third todo.",
+        completed: false,
+    },
+];
